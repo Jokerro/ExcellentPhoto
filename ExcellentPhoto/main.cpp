43d8bdb82433b4273036e7ab5aa734fa433b7256
jokerro <d.jokerro@gmail.com>
2014-11-27 12:07:11 +0200

    Обработка всех изображений в папке

--- ExcellentPhoto/main.cpp
+++ ExcellentPhoto/main.cpp
@@ -5,98 +5,124 @@
 #include <opencv2\highgui\highgui.hpp>
 
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
+#include <boost\filesystem.hpp>
 
 using namespace std;
 using namespace cv;
+using namespace boost::filesystem;
 
+void checkPhoto(const char*, const char*);
 
 int main(int argc, char* argv[])
 {
+	
+	if (argc == 2){
+		path p(argv[1]);   // p reads clearer than argv[1] in the following code
+
+		try
+		{
+			if (exists(p))    // does p actually exist?
+			{
+				if (is_regular_file(p))        // is p a regular file?   
+					cout << p << " size is " << file_size(p) << '\n';
+
+				else if (is_directory(p))      // is p a directory?
+				{
+					cout << p << " is a directory containing:\n";
+
+					typedef vector<path> vec;             // store paths,
+					vec v;                                // so we can sort them later
+
+					copy(directory_iterator(p), directory_iterator(), back_inserter(v));
+
+					sort(v.begin(), v.end());             // sort, since directory iteration
+					// is not ordered on some file systems
+					
+					for (vec::const_iterator it(v.begin()); it != v.end(); ++it)
+					{
+						
+						if (it->extension() == ".jpg")
+							checkPhoto(it->string().c_str(), it->filename().string().c_str());
+					}
+				}
+
+				else
+					cout << p << " exists, but is neither a regular file nor a directory\n";
+			}
+			else
+				cout << p << " does not exist\n";
+		}
+
+		catch (const filesystem_error& ex)
+		{
+			cout << ex.what() << '\n';
+		}
+
+	}
+	system("pause");
+	return 0;
+}
+
+void checkPhoto(const char* fileName, const char* justName){
+
+
+	IplImage* image = cvLoadImage(fileName, 1);
+	IplImage* src = 0;
+	src = cvCloneImage(image);
+	printf("[i] image: %s\n", fileName);
+	assert(src != 0);
+
 	String face_cascade_name = "haarcascade_frontalface_alt.xml";
-	String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+	String eyes_cascade_name = "parojos.xml";
 	CascadeClassifier face_cascade;
 	CascadeClassifier eyes_cascade;
-	if (!face_cascade.load(face_cascade_name)){ printf("--(!)Error loading\n"); return -1; };
-	if (!eyes_cascade.load(eyes_cascade_name)){ printf("--(!)Error loading\n"); return -1; };
+	if (!face_cascade.load(face_cascade_name)){ printf("--(!)Error loading\n"); return; };
+	if (!eyes_cascade.load(eyes_cascade_name)){ printf("--(!)Error loading\n"); return; };
 
 
-	// получаем любую подключённую камеру
-	CvCapture* capture = cvCreateCameraCapture(CV_CAP_ANY); //cvCaptureFromCAM( 0 );
-	assert(capture);
+	Mat frame1 = image;
+	std::vector<Rect> faces;
+	Mat frame_gray;
 
+	cvtColor(frame1, frame_gray, CV_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+
+	//-- Detect faces
 	
-	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 640);//1280); 
-	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480);//960); 
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(80, 80));
+	
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
+		ellipse(frame1, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(0, 0, 0), 4, 8, 0);
 
-	// узнаем ширину и высоту кадра
-	double width = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
-	double height = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
-	printf("[i] %.0f x %.0f\n", width, height);
+		Mat faceROI = frame_gray(faces[i]);
+		std::vector<Rect> eyes;
 
-	IplImage* frame = 0;
+		//-- In each face, detect eyes
+		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(35, 16));
 
-	cvNamedWindow("capture", CV_WINDOW_AUTOSIZE);
-
-	printf("[i] press Enter for capture image and Esc for quit!\n\n");
-
-	int counter = 0;
-	char filename[512];
-
-	while (true){
-		// получаем кадр
-		frame = cvQueryFrame(capture);
-
-
-
-
-		Mat frame1 = cvQueryFrame(capture);
-		std::vector<Rect> faces;
-		Mat frame_gray;
-
-		cvtColor(frame1, frame_gray, CV_BGR2GRAY);
-		equalizeHist(frame_gray, frame_gray);
-
-		//-- Detect faces
-		face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(80, 80));
-
-		for (size_t i = 0; i < faces.size(); i++)
+		for (size_t j = 0; j < eyes.size(); j++)
 		{
-			Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
-			ellipse(frame1, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
 
-			Mat faceROI = frame_gray(faces[i]);
-			std::vector<Rect> eyes;
-
-			//-- In each face, detect eyes
-			eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(40, 40));
-
-			for (size_t j = 0; j < eyes.size(); j++)
-			{
-				Point center(faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5);
-				int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
-				circle(frame1, center, radius, Scalar(255, 0, 0), 4, 8, 0);
-			}
-		}
-
-
-		// показываем
-		cvShowImage("capture", frame);
-
-		char c = cvWaitKey(33);
-		if (c == 27) { // нажата ESC
-			break;
-		}
-		else if (c == 13) { // Enter
-			// сохраняем кадр в файл
-			sprintf(filename, "Image%d.jpg", counter);
-			printf("[i] capture... %s\n", filename);
-			cvSaveImage(filename, frame);
-			counter++;
+			Point center(faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5);
+			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+			circle(frame1, center, radius, Scalar(255, 0, 0), 4, 8, 0);
 		}
 	}
-	// освобождаем ресурсы
-	cvReleaseCapture(&capture);
-	cvDestroyWindow("capture");
-	return 0;
+
+
+	char filename[256];
+	
+	sprintf(filename, justName);
+	cvSaveImage(filename, image);
+
+
+
+
+	cvReleaseImage(&image);
+	cvReleaseImage(&src);
+
 }
